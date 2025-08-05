59e5d130a66e12425a24d4a0ecba7f59a51c36f1
Mahery RAZAFIMANANTSOA <maheryrazafimanantsoa55@gmail.com>
2025-08-05 06:39:23 +0000

    Feat:  Depatement

--- src/employee_management/Department.java
+++ src/employee_management/Department.java
@@ -12,4 +12,42 @@ public class Department {
      * TODO : for our HR officer, give a function to show all details about employees, including
      * their pay.
      */
+
+    private String name;
+    private Set<FullTimeEmployee> fullTimeEmployees;
+    private Set<PartTimeEmployee> partTimeEmployees;
+
+    public Department(String name) {
+        this.name = name;
+        this.fullTimeEmployees = new HashSet<>();
+        this.partTimeEmployees = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addFullTimeEmployee(FullTimeEmployee employee) {
+        if (fullTimeEmployees.stream().noneMatch(e -> e.employeeId == employee.employeeId && e.name.equals(employee.name))) {
+            fullTimeEmployees.add(employee);
+        }
+    }
+
+    public void addPartTimeEmployee(PartTimeEmployee employee) {
+        if (partTimeEmployees.stream().noneMatch(e -> e.employeeId == employee.employeeId && e.name.equals(employee.name))) {
+            partTimeEmployees.add(employee);
+        }
+    }
+
+    public void showAllEmployees() {
+        System.out.println("Full Time Employees:");
+        for (FullTimeEmployee employee : fullTimeEmployees) {
+            System.out.println("ID: " + employee.employeeId + ", Name: " + employee.name + ", Monthly Salary: " + employee.calculatePay());
+        }
+
+        System.out.println("Part Time Employees:");
+        for (PartTimeEmployee employee : partTimeEmployees) {
+            System.out.println("ID: " + employee.employeeId + ", Name: " + employee.name + ", Hours Worked: " + employee.hoursWorked + ", Hourly Rate: " + employee.hourlyRate + ", Pay: " + employee.calculatePay());
+        }
+    
 }
